ccff5978a70d84a0c7de9b5d950a55c1175206ff
Mike Niebling <michael.niebling@gmail.com>
2019-05-14 20:30:37 -0500

    Readme & formatting

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Raytrace
+## raytrace
+
+Working through Jamis Buck's [The Ray Tracer Challenge](https://pragprog.com/book/jbtracer/the-ray-tracer-challenge)
+with TypeScript.
+
+#### Project Setup
+
+Prerequisites: You'll need Node and NPM installed on your machine.
+
+Set up the project by cloning the repo & running `npm install`.
+Run tests with `npm test`.
--- src/tuples.ts
+++ src/tuples.ts
@@ -37,24 +37,14 @@ export class Tuple {
 
 export class Point extends Tuple {
 
-  constructor (
-    x: number,
-    y: number,
-    z: number
-  ) {
-
+  constructor (x: number, y: number, z: number) {
     super(x, y, z, 1.0)
   }
 }
 
 export class Vector extends Tuple {
 
-  constructor (
-    x: number,
-    y: number,
-    z: number
-  ) {
-
+  constructor (x: number, y: number, z: number) {
     super(x, y, z, 0.0)
   }
 }
