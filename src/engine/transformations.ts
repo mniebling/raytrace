80a385c52e028d25f3329dc06091b6e2f2395928
Mike Niebling <michael.niebling@gmail.com>
2019-06-22 12:52:48 -0500

    Implement scaling transform

--- src/engine/transformations.ts
+++ src/engine/transformations.ts
@@ -1,12 +1,28 @@
 import { Matrix } from './matrix'
 
 
-export function translation(x: number, y: number, z: number): Matrix {
+export function scale (x: number, y: number, z: number): Matrix {
 
-  // translation(x, y, z) = [1 0 0 x]
-  //                        [0 1 0 y]
-  //                        [0 0 1 z]
-  //                        [0 0 0 1]
+  // scalw(x, y, z) = [x 0 0 0]
+  //                  [0 y 0 0]
+  //                  [0 0 z 0]
+  //                  [0 0 0 1]
+
+  const transform = new Matrix()
+
+  transform.setValueAt(0, 0, x)
+  transform.setValueAt(1, 1, y)
+  transform.setValueAt(2, 2, z)
+
+  return transform
+}
+
+export function translate (x: number, y: number, z: number): Matrix {
+
+  // translate(x, y, z) = [1 0 0 x]
+  //                      [0 1 0 y]
+  //                      [0 0 1 z]
+  //                      [0 0 0 1]
 
   const transform = new Matrix()
 
--- test/transformations.spec.ts
+++ test/transformations.spec.ts
@@ -1,14 +1,43 @@
 import { multiply } from '@/engine/math-general'
 import { inverse } from '@/engine/math-matrices'
-import { translation } from '@/engine/transformations'
+import { scale, translate } from '@/engine/transformations'
 import { Point, Vector } from '@/engine/tuples'
+
+
+describe('.scale()', () => {
+
+  it('should scale a point', () => {
+
+    const transform = scale(2, 3, 4)
+    const p = new Point(-4, 6, 8)
+
+    expect(multiply(transform, p)).toBeDeepCloseTo(new Point(-8, 18, 32))
+  })
+
+  it('should scale a vector', () => {
+
+    const transform = scale(2, 3, 4)
+    const v = new Vector(-4, 6, 8)
+
+    expect(multiply(transform, v)).toBeDeepCloseTo(new Vector(-8, 18, 32))
+  })
+
+  test('multiplying by an inverse transform scales in reverse', () => {
+
+    const transform = scale(2, 3, 4)
+    const invT = inverse(transform)
+    const v = new Vector(-4, 6, 8)
+
+    expect(multiply(invT, v)).toBeDeepCloseTo(new Vector(-2, 2, 2))
+  })
+})
 
 
 describe('.translate()', () => {
 
   it('should transform a point', () => {
 
-    const transform = translation(5, -3, 2)
+    const transform = translate(5, -3, 2)
     const p = new Point(-3, 4, 5)
 
     expect(multiply(transform, p)).toBeDeepCloseTo(new Point(2, 1, 7))
@@ -16,7 +45,7 @@ describe('.translate()', () => {
 
   it('should not transform a vector', () => {
 
-    const transform = translation(5, -3, 2)
+    const transform = translate(5, -3, 2)
     const v = new Vector(-3, 4, 5)
 
     expect(multiply(transform, v)).toBeDeepCloseTo(v)
@@ -24,7 +53,7 @@ describe('.translate()', () => {
 
   test('multiplying by an inverse transform moves points in reverse', () => {
 
-    const transform = translation(5, -3, 2)
+    const transform = translate(5, -3, 2)
     const p = new Point(-3, 4, 5)
     const invT = inverse(transform)
 
